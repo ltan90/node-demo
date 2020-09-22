2ee02497714360caaa432339bd7e51187c15434f
ltan <ltan.it9x@gmail.com>
2020-09-22 09:43:53 +0700

    Add fiex

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 4000;
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.send('Hello World!!'));
 app.listen(port, () => console.log('Server in running on port ' + port));
